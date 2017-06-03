bebe435501cfdac16bc64b9dd0d8e9a2bf928eed
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2017-06-03 00:49:53 -0700

    adds api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    games: null,
+    count: 0,
+    message: ''
+  }
+
+  componentDidMount() {
+    axios.get('https://jzulvmdhac.execute-api.us-west-1.amazonaws.com/dev/today')
+    .then((res) => this.setState({
+      games: res.data.games,
+      count: res.data.count,
+      message: res.data.message
+    }))
+  }
+
   render() {
+    const {games, message, count} = this.state;
+
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Huslin</h2>
+          <p>{games ? message : "..."}</p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul className="App-intro">
+          {count > 0 &&
+            games.map((game) => (
+              <li style={{listStyleType: 'none'}} key={game.id}>
+                {game.name} {game.standard_start_time}
+              </li>
+          ))}
+        </ul>
       </div>
     );
   }
